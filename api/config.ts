9ad45b5f3f1584fff856560e1170d7543a825df8
Tu Pham <71238372+phamthanhtu-9x@users.noreply.github.com>
2023-08-16 11:31:26 +0700

    upd config api

--- api/config.ts
+++ api/config.ts
@@ -5,10 +5,10 @@ interface ApiProps {
   method: any,
 }
 
-export const api = (props: ApiProps) => {
+export const api = async (props: ApiProps) => {
   const {url, method} = props;
 
-  return $fetch(`${runtimeConfig.public.apiBase + '/api' + url} + `, {
+  return await $fetch(`${runtimeConfig.public.apiBase + '/api' + url} + `, {
     method,
     headers: {
       Authorization: `Bearer ${data.value?.access_token}`,
